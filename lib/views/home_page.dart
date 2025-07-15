a5cc9516fcdade9f85b0d9edc6d62f8be0cd919c
Ahmed Bahaa <ab3854093@gamil.com>
2025-07-15 21:20:21 +0300

    رسالة التعديل

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
-
-class notes extends StatelessWidget {
-  const notes({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              size: 30,
+            ),
+          ),
+        ],
+        title: const Text(
+          'Notes',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      body: const NotesViewBody(),
+    );
   }
 }
---
+++ lib/views/widgets/notes_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 12),
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: const Padding(
+              padding: EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                'Build Your Career With Ahmed Bahaa',
+                style: TextStyle(color: Colors.black, fontSize: 18),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 30,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: Text(
+              'May21, 2025',
+              style: TextStyle(color: Colors.black, fontSize: 16),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
